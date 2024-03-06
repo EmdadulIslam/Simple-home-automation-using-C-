71a26a27e97a04763d031512df7b257cfcffad95
EmdadulIslam <sazzad.emdad@gmail.com>
2024-03-06 08:22:22 +0100

    Home automation

---
+++ HomeAutomatin/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace HomeAutomatin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstCommands = new System.Windows.Forms.ListBox();
+            this.TmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lstCommands
+            // 
+            this.lstCommands.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lstCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstCommands.ForeColor = System.Drawing.SystemColors.Window;
+            this.lstCommands.FormattingEnabled = true;
+            this.lstCommands.Location = new System.Drawing.Point(0, 0);
+            this.lstCommands.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstCommands.Name = "lstCommands";
+            this.lstCommands.Size = new System.Drawing.Size(657, 292);
+            this.lstCommands.TabIndex = 0;
+            this.lstCommands.SelectedIndexChanged += new System.EventHandler(this.lstCommands_SelectedIndexChanged);
+            // 
+            // TmrSpeaking
+            // 
+            this.TmrSpeaking.Enabled = true;
+            this.TmrSpeaking.Interval = 1000;
+            this.TmrSpeaking.Tick += new System.EventHandler(this.TmrSpeaking_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(657, 292);
+            this.Controls.Add(this.lstCommands);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCommands;
+        private System.Windows.Forms.Timer TmrSpeaking;
+    }
+}
+
